c7f13043ea326d10c60f79fb7a31754b472cf43c
Dibya Ranjan Pradhan <168287577+Dibya0912@users.noreply.github.com>
2025-10-18 19:54:38 +0530

    Add singleton DB connection implementation
    Implement singleton pattern for database connection.

--- src/main/java/dao/DBConnection.java
+++ src/main/java/dao/DBConnection.java
@@ -1,9 +1,26 @@
 package dao;
 
-
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class DBConnection {
 	
+	private static Connection connection;
 
+    public static Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/cinema_db",
+                        "root",  
+                        "Ritesh@2005" 
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 
 }
